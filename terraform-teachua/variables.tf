f824db5f81e6cdcaef618a522cea316baf250821
Oleksandr Aleksyeyv <150668618+joeblackA@users.noreply.github.com>
2025-08-12 22:58:07 +0300

    Update variables.tf

--- terraform-teachua/variables.tf
+++ terraform-teachua/variables.tf
@@ -42,10 +42,10 @@ variable "gcp_project" {
   type = string
 }
 
-variable "gcp_region" {
-  type    = string
-  default = "us-central1"
-}
+#variable "gcp_region" {
+ # type    = string
+  #default = "us-central1"
+#}
 
 variable "aws_region" {
   type    = string
