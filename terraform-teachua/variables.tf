dd8cc45378ec228bd84beec0600c36d971f0b16c
Oleksandr Aleksyeyv <150668618+joeblackA@users.noreply.github.com>
2025-08-12 22:47:58 +0300

    Update variables.tf

--- terraform-teachua/variables.tf
+++ terraform-teachua/variables.tf
@@ -52,7 +52,7 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "gcp_credentials_file" {
-  description = "Path to the GCP service account key file"
-  type        = string
-}
+#variable "gcp_credentials_file" {
+ # description = "Path to the GCP service account key file"
+  #type        = string
+#}
