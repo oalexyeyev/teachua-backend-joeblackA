eb432283532ff37190c20730612c4e27624e3f69
Oleksandr Aleksyeyv <150668618+joeblackA@users.noreply.github.com>
2025-08-14 12:10:52 +0300

    Create backend.tf

---
+++ terraform-teachua/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket      = "teachua-terraform1"
+    key         = "terraform.tfstate"
+    region      = "us-east-1"
+    profile     = "terraform-tfstate"
+    encrypt     = true
+    use_lockfile = true    # instead of dynamodb_table
+  }
+}
