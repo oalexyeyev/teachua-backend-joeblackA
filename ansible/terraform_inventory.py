04973d93663693d6a0a1b1813170a072bd68da1e
Oleksandr Aleksyeyv <150668618+joeblackA@users.noreply.github.com>
2025-08-14 16:06:15 +0300

    Update terraform_inventory.py

--- ansible/terraform_inventory.py
+++ ansible/terraform_inventory.py
@@ -27,6 +27,7 @@ def main():
             "ansible_host": bastion_public_ip,
             "ansible_user": bastion_user,
             "ansible_ssh_private_key_file": ssh_key_path
+            "ansible_ssh_common_args": f"-o StrictHostKeyChecking=no"
         }
         inventory["all"].append(bastion_public_ip)
 
