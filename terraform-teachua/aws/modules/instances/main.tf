363bd9d6462cebe71e8938a7696f05f2625f0095
Oleksandr Aleksyeyv <150668618+joeblackA@users.noreply.github.com>
2025-08-18 12:48:14 +0300

    Update main.tf

--- terraform-teachua/aws/modules/instances/main.tf
+++ terraform-teachua/aws/modules/instances/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "bastion" {
   ami                    = "ami-0de716d6197524dd9" # Amazon Linux 2 us-east-1, заміни якщо потрібно
-  instance_type          = "t3.micro"
+  instance_type          = "t2.micro"
   subnet_id              = var.public_subnet_id
   key_name               = var.key_pair_name
   vpc_security_group_ids = [var.bastion_sg_id]
@@ -12,7 +12,7 @@ resource "aws_instance" "bastion" {
 
 resource "aws_instance" "backend" {
   ami                    = "ami-0de716d6197524dd9"
-  instance_type          = "t3.micro"
+  instance_type          = "t2.micro"
   subnet_id              = var.private_subnet_id
   key_name               = var.key_pair_name
   vpc_security_group_ids = [var.backend_sg_id]
